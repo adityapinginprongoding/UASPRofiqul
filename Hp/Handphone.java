2b3ae44ddf79a18128b116d0e88d0fdde4611c01
adityapinginprongoding <adityakeche567@gmail.com>
2025-01-03 23:36:19 +0700

    Update hp

--- Hp/Handphone.java
+++ Hp/Handphone.java
@@ -10,18 +10,18 @@ package Hp;
  */
 public class Handphone {
     public void hidupkan() {
-        System.out.println("Handphone hidup...");
+        System.out.println("Handphone hidup....");
     }
 
     public void lakukanPanggilan() {
-        System.out.println("Kring, kring, kring...panggilan dilakukan");
+        System.out.println("Kring, kring, kring....panggilan dilakukan");
     }
 
     public void kirimSMS() {
-        System.out.println("Dung, dung... sms berhasil terkirim");
+        System.out.println("Dung, dung.... sms berhasil terkirim");
     }
 
     public void matikan() {
-        System.out.println("Handphone mati...");
+        System.out.println("Handphone mati....");
     }
 }
